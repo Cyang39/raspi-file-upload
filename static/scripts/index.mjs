818e9343af92d88767fedba16c00cf3c96aa2418
Cyang39 <huqinyang123@gmail.com>
2019-10-04 21:59:19 +0800

    use elementui

--- static/scripts/index.mjs
+++ static/scripts/index.mjs
@@ -3,16 +3,20 @@ import { pGet, getQueryVariable } from './utils.mjs'
 new Vue({
   template:
     `<div>
-    <div v-for="(item, index) in list">
-      <div v-if="item.isDir">
-        <div style="width: 20em;display:inline-block;">【目录】{{item.name}}</div>
-        <a :href="'/index.html?path=' + path + item.name + '/'">进入</a>
-      </div>
-      <div v-if="item.isFile">
-        <div style="width: 20em;display:inline-block;">【文件】{{item.name}}</div>
-        <a :href="'/api/download?path=' + path + item.name">下载</a>
-      </div>
+    <div v-for="(item, index) in list" :key="index">
+      <el-link v-if="item.isDir" :href="'/index.html?path=' + path + item.name + '/'" type="primary">【目录】{{item.name}}</el-link>
+      <el-link v-if="item.isFile" :href="'/api/download?path=' + path + item.name" type="primary">【文件】{{item.name}}</el-link>
     </div>
+
+    <el-upload
+      class="upload-demo"
+      drag
+      :action="'/api/upload?path=' + path"
+      multiple>
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+    </el-upload>
+  
   </div>`,
   el: '#app',
   data: {
