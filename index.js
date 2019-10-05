9c8194beeb36a92b1782d4558a157333e829e381
Cyang39 <huqinyang123@gmail.com>
2019-10-05 09:53:42 +0800

    delete server render(view engine), using C/S model

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const formidable = require('formidable')
-const util = require('util')
 const express = require('express')
-const fs = require('fs')
 const config = require('yargs')
   .alias('uploadDir', 'path')
   .argv
-
-const t = require('./lib/utils')
 
 // 默认配置
 config.uploadDir = config.uploadDir || __dirname
@@ -20,71 +15,8 @@ app.use((req, res, next) => {
   next();
 })
 
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.redirect('/fs/'))
-app.get('/*', express.static(__dirname+'/static'))
-app.get('/fs/*', express.static(config.uploadDir, { dotfiles: 'allow' }))
-app.get('/web', (req, res) => res.sendFile(__dirname + '/static/index.html'))
-
+app.get('/*', express.static(__dirname + '/static'))
 
 app.use('/api', require('./lib/routers/api'))
-
-app.all('/fs/*', (req, res, next) => {
-  req.target = decodeURIComponent(req.path.substring(3))
-  req.file_path = config.uploadDir + req.target
-  next()
-})
-
-app.get('/fs/*', function (req, res) {
-  // 文件不存在
-  if(!fs.existsSync(req.file_path)) {
-    res.render('error', {message: `No such file or folder "${req.target}"`})
-    return
-  }
-  // 删除文件
-  if(req.query.operate === 'delete' || req.query.operate === 'del') {
-    if(!fs.statSync(req.file_path).isFile()) return res.render('error', {message: `目前无法删除目录`})
-    fs.unlinkSync(req.file_path)
-    return res.redirect('./')
-  }
-  // 下载文件
-  if(fs.statSync(req.file_path).isFile()) {
-    res.sendFile(req.file_path)
-    return
-  }
-  // 进入目录
-  if(fs.statSync(req.file_path).isDirectory()) {
-    if(req.path[req.path.length - 1] !== '/') return res.redirect(req.path + '/')
-
-    const children = fs.readdirSync(req.file_path).map(x => {
-        const res = {name:x, url:req.path + x}
-        const stat = fs.statSync(req.file_path+x)
-        res.type = stat.isDirectory() ? 'dir' : stat.isFile() ? 'file' : 'unkonw'
-        res.size = t.formatFileSize(stat.size)
-        return res
-      })
-    res.render('index', { title: req.target, list: children })
-  }
-
-})
-
-// 上传文件
-app.post('/upload', function(req, res) {
-  var form = new formidable.IncomingForm()
-  form.maxFileSize = 4 * 1000 * 1000 * 1000
-
-  form.on('field', function(name, value) {
-    if(!value) value = config.uploadDir
-    form.on('fileBegin', function(name, file) {
-      file.path = config.uploadDir + value + t.decodeEntities(file.name);
-    })
-  });
-
-  form.parse(req, function(err, fields, files) {
-    res.writeHead(200, {'content-type': 'text/plain'})
-    res.write('received upload:\n\n')
-    res.end(util.inspect({fields: fields, files: files}))
-  })
-})
 
 app.listen(config.port)
--- static/scripts/index.mjs
+++ static/scripts/index.mjs
@@ -3,10 +3,20 @@ import { pGet, getQueryVariable } from './utils.mjs'
 new Vue({
   template:
     `<div>
-    <div v-for="(item, index) in list" :key="index">
+    <el-page-header @back="goBack" :content="'当前路径：' + path">
+    </el-page-header>
+
+    <el-row v-for="(item, index) in list" :key="index" style="margin:.1em">
+    <el-col :span="6">
       <el-link v-if="item.isDir" :href="'/index.html?path=' + path + item.name + '/'" type="primary">【目录】{{item.name}}</el-link>
       <el-link v-if="item.isFile" :href="'/api/download?path=' + path + item.name" type="primary">【文件】{{item.name}}</el-link>
-    </div>
+    </el-col>
+    <el-col :span="6">
+      <el-button type="danger" size="mini" @click="deleteItem(item.name)">删除</el-button>
+    </el-col>
+    </el-row>
+    
+    <el-button>新建文件夹</el-button>
 
     <el-upload
       class="upload-demo"
@@ -17,6 +27,7 @@ new Vue({
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
     </el-upload>
   
+    
   </div>`,
   el: '#app',
   data: {
@@ -27,8 +38,35 @@ new Vue({
     this.path = getQueryVariable('path') || "/"
   },
   watch: {
-    async path(val) {
-      this.list = await pGet('/api/ls?path=' + val)
+    path() {
+      this.updateList()
+    }
+  },
+  methods: {
+    async updateList() {
+      this.list = await pGet('/api/ls?path=' + this.path)
+    },
+    deleteItem(name) {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        console.log(await pGet('/api/delete?path=' + this.path + name))
+        this.updateList()
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });          
+      });
+    },
+    goBack() {
+      console.log("back")
     }
   }
 })
