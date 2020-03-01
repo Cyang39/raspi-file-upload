906f64c554955b9069db1911ef3a197c5680cd42
cyang39 <huqinyang123@gmail.com>
2020-03-01 20:23:39 +0800

    add new target

--- static/scripts/pages/file_manager/template.mjs
+++ static/scripts/pages/file_manager/template.mjs
@@ -9,13 +9,11 @@ export default `
     <span style="color:#ddd">|&nbsp;&nbsp;&nbsp;</span>
     <el-link disabled type="info">{{path}}</el-link>
 
-    <hr>
-
     <div v-if="list.length === 0">
       <span style="color:#ddd">该目录为空</span>
     </div>
 
-    <div v-for="(item, index) in list" :key="index">
+    <div v-for="(item, index) in list" :key="index" :style="{backgroundColor:index%2==0?'snow':'white'}">
       <span style="width:450px;display:inline-block;">
         <el-link v-if="item.isDir" @click="changeDir(item.name)" type="warning"><i class="el-icon-folder"></i> {{item.name}}</el-link>
         <el-link v-if="item.isFile" disabled type="primary"><i class="el-icon-document"></i> {{item.name}}</el-link>
@@ -29,8 +27,6 @@ export default `
       
       <el-link v-if="item.isFile" :href="'/api/download?path=' + path + item.name" type="primary">下载</el-link>
     </div>
-    
-    <hr>
 
     <el-upload drag :action="'/api/upload?path=' + path" multiple>
       <i class="el-icon-upload"></i>
